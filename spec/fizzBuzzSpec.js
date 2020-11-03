6e0d703174f76a588f08c710ac39aee4afb70967
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-03 15:26:39 +0000

    Added condition for number divisible by 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -6,19 +6,23 @@ describe("FizzBuzz", function() {
     fizzBuzz = new FizzBuzz();
   });
 
+  it("checks that the number twenty-five is outputs as BUZZ", function() {
+
+    expect(fizzBuzz.print(25)).toEqual('buzz');
+  });
+
   it("checks that the number three is outputs as FIZZ", function() {
 
     expect(fizzBuzz.print(3)).toEqual('fizz');
   });
 
-  it("checks that the number five is outputs as BUZZ", function() {
-
-    expect(fizzBuzz.print(5)).toEqual('buzz');
-  });
-
   it('checks that the number 15 is outputs as FizzBuzz', function() {
 
     expect(fizzBuzz.print(15)).toEqual('fizzbuzz');
+  });
+
+  it('checks that the number 12 outputs as FIZZ', function() {
+    expect(fizzBuzz.print(12)).toEqual('fizz');
   });
 
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -2,10 +2,12 @@ class FizzBuzz {
 
 
   print(number) {
-    if (number === 5) {
-      return "buzz";
+    if (number === 15) {
+      return "fizzbuzz";
     } else if (number === 3) {
       return "fizz";
+    } else if (number % 5 === 0) {
+      return 'buzz';
     }
   }
 }
