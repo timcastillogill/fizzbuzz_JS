eabf9df9f1ad304498983f545327f3c0d241e89b
jotkafomat <k-j@xl.wp.pl>
2020-11-03 15:53:53 +0000

    print numbers form 100 to 1

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -8,6 +8,13 @@ class FizzBuzz {
       return "fizz";
     } else if (number % 5 === 0) {
       return 'buzz';
+    } else {
+      return number;
     }
   }
+}
+var fizzBuzz = new FizzBuzz();
+
+for (let i = 100; i >= 1; i--) {
+  console.log(fizzBuzz.print(i));
 }
