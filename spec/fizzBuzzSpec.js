bebd0776f0bc2cb82bf59398e711afd5ede668bd
jotkafomat <k-j@xl.wp.pl>
2020-11-03 15:03:38 +0000

    add test for number 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -11,9 +11,14 @@ describe("FizzBuzz", function() {
     expect(fizzBuzz.print(3)).toEqual('fizz');
   });
 
-  it("checks that the number three is outputs as BUZZ", function() {
+  it("checks that the number five is outputs as BUZZ", function() {
 
     expect(fizzBuzz.print(5)).toEqual('buzz');
+  });
+
+  it('checks that the number 15 is outputs as FizzBuzz', function() {
+
+    expect(fizzBuzz.print(15)).toEqual('fizzbuzz');
   });
 
 });
