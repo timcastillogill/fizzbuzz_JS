446505436de50daf646cdab1730514a79b81c4d5
jotkafomat <k-j@xl.wp.pl>
2020-11-03 14:51:10 +0000

    mvp passing

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,9 +1,10 @@
 class FizzBuzz {
 
-  function print(number) {
+  print(number) {
     if (number === 5) {
       return "buzz";
     } else if (number === 3) {
       return "fizz";
-    };
-  };
+    }
+  }
+}
